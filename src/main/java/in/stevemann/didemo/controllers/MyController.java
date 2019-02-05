9b97d2873b04cb5ee6a6f3d74d7de8c5bfce168d
Steve Mann <stevemann2705@gmail.com>
2019-02-05 23:37:26 +0530

    Adding Mock Controller

---
+++ src/main/java/in/stevemann/didemo/controllers/MyController.java
@@ -0,0 +1,11 @@
+package in.stevemann.didemo.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+    public String hello(){
+        System.out.println("Hello!!!");
+        return "Hello";
+    }
+}
