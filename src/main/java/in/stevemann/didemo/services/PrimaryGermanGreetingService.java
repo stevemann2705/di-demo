099a996f1e73e634c25102bfe7293feeaf7b1655
Steve Mann <stevemann2705@gmail.com>
2019-02-06 14:38:35 +0530

    Adding German Profile

---
+++ src/main/java/in/stevemann/didemo/services/PrimaryGermanGreetingService.java
@@ -0,0 +1,15 @@
+package in.stevemann.didemo.services;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+@Service
+@Primary
+@Profile("de")
+public class PrimaryGermanGreetingService implements GreetingService {
+    @Override
+    public String sayGreeting() {
+        return "Primäre Begrüßung";
+    }
+}
