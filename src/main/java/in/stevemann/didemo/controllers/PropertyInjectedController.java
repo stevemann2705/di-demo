57b395c9126b2fdb5bb6730cf89cf948c3b57039
Steve Mann <stevemann2705@gmail.com>
2019-02-06 14:11:27 +0530

    @Primary overrides the variable name injection technique

--- src/main/java/in/stevemann/didemo/controllers/PropertyInjectedController.java
+++ src/main/java/in/stevemann/didemo/controllers/PropertyInjectedController.java
@@ -2,11 +2,13 @@ package in.stevemann.didemo.controllers;
 
 import in.stevemann.didemo.services.GreetingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class PropertyInjectedController {
     @Autowired
+    @Qualifier("greetingServiceImpl")
     public GreetingService greetingServiceImpl;
 
     public String sayHello(){
