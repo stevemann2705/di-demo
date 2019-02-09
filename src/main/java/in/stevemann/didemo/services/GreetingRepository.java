9f192558ffdfc99868fcd255943c00c8c227771a
Steve Mann <stevemann2705@gmail.com>
2019-02-09 16:46:58 +0530

    Adding Greeting Repository

---
+++ src/main/java/in/stevemann/didemo/services/GreetingRepository.java
@@ -0,0 +1,10 @@
+package in.stevemann.didemo.services;
+
+public interface GreetingRepository {
+
+    String getEnglishGreeting();
+
+    String getSpanishGreeting();
+
+    String getGermanGreeting();
+}
--- src/main/java/in/stevemann/didemo/services/PrimaryGreetingService.java
+++ src/main/java/in/stevemann/didemo/services/PrimaryGreetingService.java
@@ -8,8 +8,15 @@ import org.springframework.stereotype.Service;
 @Primary
 @Profile({"en", "default"})
 public class PrimaryGreetingService implements GreetingService {
+
+    private GreetingRepository greetingRepository;
+
+    public PrimaryGreetingService(GreetingRepository greetingRepository) {
+        this.greetingRepository = greetingRepository;
+    }
+
     @Override
     public String sayGreeting() {
-        return "Hello - This is primary greeting service";
+        return greetingRepository.getEnglishGreeting();
     }
 }
