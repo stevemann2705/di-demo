4fc258e7a73b19db3ba2aa1c944cc4f2eac32683
Steve Mann <stevemann2705@gmail.com>
2019-02-11 01:23:27 +0530

    Using Environment Properties

--- src/main/java/in/stevemann/didemo/config/PropertyConfig.java
+++ src/main/java/in/stevemann/didemo/config/PropertyConfig.java
@@ -1,15 +1,20 @@
 package in.stevemann.didemo.config;
 
 import in.stevemann.didemo.examplebeans.FakeDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
 
 @Configuration
 @PropertySource("classpath:datasource.properties")
 public class PropertyConfig {
+
+    @Autowired
+    Environment env;
 
     @Value("${in.username}")
     String user;
@@ -25,7 +30,7 @@ public class PropertyConfig {
         FakeDataSource fakeDataSource = new FakeDataSource();
         fakeDataSource.setPassword(password);
         fakeDataSource.setUrl(url);
-        fakeDataSource.setUser(user);
+        fakeDataSource.setUser(env.getProperty("USERNAME"));
         return fakeDataSource;
     }
 
